c8cfd01a0cdbcd3ee4e3cd42e56379d87cf42d5b
Adrian Pardini <publico@tangopardo.com.ar>
2013-11-16 10:51:55 -0300

    updated license and authors

--- src/modules/gstshm/producer_gstshm.c
+++ src/modules/gstshm/producer_gstshm.c
@@ -3,20 +3,25 @@
  * frame data from POSIX shared memory that is compatible
  * with the GStreamer protocol.
  *
+ * Copyright © <2013> Instituto Nacional de Asociativismo y Economia Social.
+ * Copyright © <2013> Cooperativa de Trabajo OpCode Limitada <info@opcode.coop>.
+ * Copyright © <2013> Adrián Pardini     <adrian@opcode.coop>
+ * Copyright © <2013> Tomás Neme         <tomas@opcode.coop>
+ * Copyright © <2013> Patricio Reboratti <patricio@opcode.coop>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include <framework/mlt.h>
